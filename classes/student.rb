1c24d27128ce14b640fa0a2da8f7e35ce66543b9
gdumani <gdumani@gmail.com>
2023-05-16 10:58:09 -0600

    create classes for person, student and teacher

---
+++ classes/student.rb
@@ -0,0 +1,13 @@
+require './person'
+
+# Class for students, inherits from person
+class Student < Person
+  def initialize(name, age, parent_permission, classroom)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
---
+++ classes/teacher.rb
@@ -0,0 +1,13 @@
+require './person'
+
+# class for teacher inherits from person.
+class Teacher < Person
+  def initialize(name, age, specialization)
+    super(name, age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
